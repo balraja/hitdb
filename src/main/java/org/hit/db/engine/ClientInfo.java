0016fbb454b3af0ee951cb6ee4a2539ea9e884c1
balraja <balrajasubbiah@gmail.com>
2013-09-23 02:28:54 +0530

    Adding files missed in the previous commit

---
+++ src/main/java/org/hit/db/engine/ClientInfo.java
@@ -0,0 +1,60 @@
+/*
+    Hit is a high speed transactional database for handling millions
+    of updates with comfort and ease. 
+
+    Copyright (C) 2013  Balraja Subbiah
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.hit.db.engine;
+
+import org.hit.communicator.NodeID;
+
+/**
+ * A simple class to implement a structure for capturing the 
+ * client information.
+ */
+public class ClientInfo
+{
+    private final NodeID myClientID;
+    
+    private final long myClientSequenceNumber;
+
+    /**
+     * CTOR
+     */
+    public ClientInfo(NodeID clientID, long clientSequenceNumber)
+    {
+        super();
+        myClientID = clientID;
+        myClientSequenceNumber = clientSequenceNumber;
+    }
+
+    /**
+     * Returns the value of clientID
+     */
+    public NodeID getClientID()
+    {
+        return myClientID;
+    }
+
+    /**
+     * Returns the value of clientSequenceNumber
+     */
+    public long getClientSequenceNumber()
+    {
+        return myClientSequenceNumber;
+    }
+}
